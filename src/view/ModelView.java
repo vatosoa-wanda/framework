deef781f7d08dc2b728eccdf53e3ec3a5feda5cd
vatosoa-wanda <154837268+vatosoa-wanda@users.noreply.github.com>
2025-11-23 23:46:47 +0300

    sprint_5

--- src/view/ModelView.java
+++ src/view/ModelView.java
@@ -1,9 +1,38 @@
 package view;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ModelView {
     private String view;
-    public ModelView() {}
-    public ModelView(String view) { this.view = view; }
-    public String getView() { return view; }
-    public void setView(String view) { this.view = view; }
+    private Map<String, Object> model;
+
+    public ModelView() {
+        this.model = new HashMap<>();
+    }
+
+    public ModelView(String view) {
+        this.view = view;
+        this.model = new HashMap<>();
+    }
+
+    public String getView() {
+        return view;
+    }
+
+    public void setView(String view) {
+        this.view = view;
+    }
+
+    public Map<String, Object> getModel() {
+        return model;
+    }
+
+    public void setModel(Map<String, Object> model) {
+        this.model = model;
+    }
+
+    public void addObject(String key, Object value) {
+        this.model.put(key, value);
+    }
 }
